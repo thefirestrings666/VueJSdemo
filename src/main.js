e1b702a3570f45ad088f247fc765837272d59a42
thefirestrings666 <59292048+thefirestrings666@users.noreply.github.com>
2020-01-05 14:50:11 +0100

    FirebaseUpload

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -13,3 +14,5 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+
